fb8a7393638f53902ecbb9889474ba40c47ef82e
Мусин Михаил <musin.mihail@gmail.com>
2025-05-24 21:47:16 +0300

    Скрипты и обновления.

---
+++ Assets/Scripts/UI/ResourceCounterDisplay.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ResourceCounterDisplay : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
